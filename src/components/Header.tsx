9a14da3ce7571f33e4f3e4f99f221e0f9fa3a949
Rohan Balkondekar <brohan5501@gmail.com>
2023-09-16 22:21:30 +0530

    ❤️

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,7 @@ const Header = () => {
     <div className="container"> {/* Added the container div */}
       <img src={logo} className="logo" alt="Site Logo" />
       <h2 className='slogan'>Out of the Box Insights For your Business</h2>
+      <h2 className='update'>❤️</h2>
     </div>
   );
 };
