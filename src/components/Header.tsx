710efd1b9479091a45671d45f9d51d5a71a52735
Rohan Balkondekar <brohan5501@gmail.com>
2023-09-16 16:51:00 +0530

    genesis

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css';
+import logo from '../assets/Outsight.png';
+
+const Header = () => {
+  return (
+    <div className="container"> {/* Added the container div */}
+      <img src={logo} className="logo" alt="Site Logo" />
+      <h2 className='slogan'>Out of the Box Insights For your Business</h2>
+    </div>
+  );
+};
+
+export default Header;
