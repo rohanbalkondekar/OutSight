66bccdb769cd9ab57201bbeb30acf8197d159e3a
Rohan-Balkondekar-Lyzr <rohan@lyzr.ai>
2024-05-22 00:41:45 +0530

    remove unused components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import './App.css';
-import Header from './components/Header';
 import Hero from './components/Hero';
-import Action from './components/Action';
 import GoogleForm from './components/GoogleForm';
 
 function App() {
   return (
     <>
-      {/* <Header /> */}
       <Hero />
       <GoogleForm />
     </>
